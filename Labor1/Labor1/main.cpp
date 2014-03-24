fc946b10cf20dcf05d345e74c867ffd3eb0d16a9
Roland Fronczek <rolifronczek@web.de>
2014-03-24 14:56:53 +0100

    Test

---
+++ Labor1/Labor1/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Labor1
+//
+//  Created by Roland Fronczek on 24.03.14.
+//  Copyright (c) 2014 Roland Fronczek. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+

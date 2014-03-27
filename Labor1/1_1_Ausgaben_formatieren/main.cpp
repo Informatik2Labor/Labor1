2546bfb97b660bc0f60e251366cc423f6c01d123
Roland Fronczek <rolifronczek@web.de>
2014-03-27 20:58:51 +0100

    no message

--- Labor1/1_1_Ausgaben_formatieren/main.cpp
+++ Labor1/1_1_Ausgaben_formatieren/main.cpp
@@ -1,18 +1,12 @@
-//
-//  main.cpp
-//  1_1_Ausgaben_formatieren
-//
-//  Created by Marvin Wiegand on 24.03.14.
-//  Copyright (c) 2014 Roland Fronczek. All rights reserved.
-//
+/*****************************************************************
+*   Name Modul/Datei:
+*
+*   Autoren: Marvin Wiegand (...@hs-esslingen.de), Roland Fronczek (rofrit01@hs-esslingen.de)
+*   Beschreibung:
+*   Datum Entwicklungsstart:
+*   Datum Entwicklungsende:
+*   Zeitaufwand:
+*   Modifikationen:
+*******************************************************************/
 
-#include <iostream>
-
-int main(int argc, const char * argv[])
-{
-
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
-}
 

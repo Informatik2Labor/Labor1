77cadc335c5c4eae9b9cb41ccdec902b2e495b6a
Roland Fronczek <rolifronczek@web.de>
2014-03-27 22:11:43 +0100

    Kommentare teilweise aufgefüllt ( z.b. oct, hex) Noch nicht lauffähig

--- Labor1/1_1_Ausgaben_formatieren/main.cpp
+++ Labor1/1_1_Ausgaben_formatieren/main.cpp
@@ -2,7 +2,9 @@
 *   Name Modul/Datei: Aufgabe 1:Ausgaben formatieren MAIN
 *
 *   Autoren: Marvin Wiegand (...@hs-esslingen.de), Roland Fronczek (rofrit01@hs-esslingen.de)
-*   Beschreibung:
+ 
+*   Beschreibung: Variable k wird in verschiedenen formatierungen (oc, hex,...) angezeigt)
+ 
 *   Datum Entwicklungsstart:
 *   Datum Entwicklungsende:
 *   Zeitaufwand:
@@ -11,21 +13,23 @@
 
 
 #include <string> 
-#include <iomanip>
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
 int main() {
     double d = 1.234;
-    cout << ”Nr Oct Hex String Fixed
-    for (int k = 1; k < 11; k++) {
-        Scientific ” << endl;
-        ￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼cout<</*xxx*/ <<k;
-        cout<</*xxx*/ <<k;
-        cout<</*xxx*/ <<k<<””;
-        cout<</*xxx*/ <<string(k%5+1,’∗’)<<””; cout<</*xxx*/ <<d;
-        cout<</*xxx*/ <<d; d = d*-2;
+    cout << "Nr Oct Hex String Fixed Scientific << endl";
+    for (int k = 1; k < 11; k++)
+    {
+        ￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼cout << dec << k;
+        cout << oct << k;
+        cout << hex << k << ” ”;
+        cout <<  << string( k % 5 + 1 , ’∗’ ) << ” ”;
+        cout << fixed << d;
+        cout << scientific << d;
+        d = d*-2;
         cout << endl;
     }
     

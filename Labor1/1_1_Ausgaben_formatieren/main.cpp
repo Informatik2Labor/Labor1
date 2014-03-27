06f9811e6e624ecad375e261ba06441bfe085d20
Roland Fronczek <rolifronczek@web.de>
2014-03-27 21:06:24 +0100

    no message

--- Labor1/1_1_Ausgaben_formatieren/main.cpp
+++ Labor1/1_1_Ausgaben_formatieren/main.cpp
@@ -1,5 +1,5 @@
 /*****************************************************************
-*   Name Modul/Datei:
+*   Name Modul/Datei: Aufgabe 1:Ausgaben formatieren MAIN
 *
 *   Autoren: Marvin Wiegand (...@hs-esslingen.de), Roland Fronczek (rofrit01@hs-esslingen.de)
 *   Beschreibung:
@@ -10,3 +10,24 @@
 *******************************************************************/
 
 
+#include <string> 
+#include <iomanip>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    double d = 1.234;
+    cout << ”Nr Oct Hex String Fixed
+    for (int k = 1; k < 11; k++) {
+        Scientific ” << endl;
+        ￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼cout<</*xxx*/ <<k;
+        cout<</*xxx*/ <<k;
+        cout<</*xxx*/ <<k<<””;
+        cout<</*xxx*/ <<string(k%5+1,’∗’)<<””; cout<</*xxx*/ <<d;
+        cout<</*xxx*/ <<d; d = d*-2;
+        cout << endl;
+    }
+    
+    return 0;
+}

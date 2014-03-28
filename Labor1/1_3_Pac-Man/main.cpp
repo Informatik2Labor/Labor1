90d15442db914e642eecb70b5069ba3cf25a8d8f
Roland Fronczek <rolifronczek@web.de>
2014-03-28 13:48:46 +0100

    Hinzufügen Aufgabbe 1_3

---
+++ Labor1/1_3_Pac-Man/main.cpp
@@ -0,0 +1,23 @@
+/*****************************************************************
+ *   Name Modul/Datei: Aufgabe 2:Pacman Spiel MAIN
+ *
+ *   Autoren: Marvin Wiegand (mawiit13@hs-esslingen.de), Roland Fronczek (rofrit01@hs-esslingen.de)
+ 
+ *   Beschreibung: Variable k wird in verschiedenen formatierungen (oc, hex,...) angezeigt)
+ 
+ *   Datum Entwicklungsstart:
+ *   Datum Entwicklungsende:
+ *   Zeitaufwand:
+ *   Modifikationen:
+ *******************************************************************/
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+

bfac8fcd0018bb3b36f38936522d1fe44f62f5fa
Roland Fronczek <rolifronczek@web.de>
2014-04-11 14:13:05 +0200

    1_2  Array in dynamisch umgeaendert

--- Labor1/1_2_Daten_eingeben_ausgeben/main.cpp
+++ Labor1/1_2_Daten_eingeben_ausgeben/main.cpp
@@ -1,14 +1,14 @@
 /*****************************************************************
  *   Name Modul/Datei: Aufgabe 2: Daten einlesen und ausgeben MAIN
  *
- *   Autoren: Marvin Wiegand (mawiit13@hs-esslingen.de), Roland Fronczek (rofrit01@hs-esslingen.de)
+ *   Autoren: Roland Fronczek (rofrit01@hs-esslingen.de), Marvin Wiegand (mawiit13@hs-esslingen.de)
  *
  *   Beschreibung: Einlesen von gewünschter Anzahl an Zahlen in ein Array. Ausgabe aller Zahlen und des Durchschnitts des kompletten Arrays
  *
  *   Datum Entwicklungsstart: 31.03.2014
- *   Datum Entwicklungsende:
- *   Zeitaufwand:
- *   Modifikationen:
+ *   Datum Entwicklungsende: 11.4.2014
+ *   Zeitaufwand: 0,5 h
+ *   Modifikationen: Änderung des Arrays in dyn. Array
  *******************************************************************/
 
 #include <iostream>
@@ -17,7 +17,7 @@ using namespace std;
 
 int main (void)
 {
-    int anzahl, zaehler, durchschnitt;
+    int anzahl, zaehler, durchschnitt, wert;
     zaehler = 0;
     
     
@@ -26,26 +26,29 @@ int main (void)
     
     cin >> anzahl;
     
-    int aZahlen[anzahl];
+    //int aZahlen[anzahl];
+    
+    int *aZahlen = new int[anzahl];
     
     cout << "Bitte geben Sie ihre ganzzahligen Werte ein:";
     
-    //for each Schleife zur eingabe der Zahlen
-    for (int werte : aZahlen)
-    {
-        cin >> werte;
-        zaehler = zaehler + werte;
+    //for Schleife zur eingabe der Zahlen
+    for (int i=0; i<=anzahl-1; i++) {
+        cin >> wert;
+        aZahlen[i] = wert;
+        zaehler = zaehler + aZahlen[i];
     }
     
     //for each Schleife zur ausgabe der Array Elemente NOT READY
-    for (int wert : aZahlen)
-    {
-        cout << wert << endl;
+    for (int i=0; i<=anzahl-1; i++) {
+        cout << aZahlen[i] << endl;
     }
     
     durchschnitt = zaehler/anzahl;
     
     cout << "Durchschnitt des Arrays:" << durchschnitt;
+    
+    delete[] aZahlen;
     
     return 0;
 }
